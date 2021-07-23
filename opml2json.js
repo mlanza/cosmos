2270703a570bf65c7170755c3a8d851d4667b7a8
Mario T. Lanza <me@mlanza.com>
2021-07-23 15:47:13 -0400

    rewrite for deno

--- opml2json.js
+++ opml2json.js
@@ -1,46 +1,78 @@
-var fs = require('fs'),
-    path = require('path'),
-    parser = require('xml2json'),
-    flatten = require('flatten'),
-    filename = path.join(__dirname, process.argv[2]),
-    destname = filename.replace(".opml", ".json"),
-    seed = 0;
+import {parse} from "https://deno.land/x/xml/mod.ts";
+import {flatten} from "https://deno.land/x/denodash@v0.1.3/src/array/flatten.ts";
 
-function read(source){
+const [src] = Deno.args;
+const dest = src.replace(".opml", ".json");
+const uids = Deno.args.indexOf("--uids") > -1;
+
+Deno.readTextFile(src).
+  then(parse).
+  then(transform).
+  then(stringify).
+  then(write(dest));
+
+function seed(start){
+  let n = start;
+  return function(){
+    return n++;
+  }
+}
+
+function uid() {
+  let firstPart = (Math.random() * 46656) | 0;
+  let secondPart = (Math.random() * 46656) | 0;
+  firstPart = ("000" + firstPart.toString(36)).slice(-3);
+  secondPart = ("000" + secondPart.toString(36)).slice(-3);
+  return firstPart + secondPart;
+}
+
+const next = uids ? uid : seed(0);
+
+function stringify(json){
+  return JSON.stringify(json, null, "\t");
+}
+
+function write(dest){
+  return function(text){
+    Deno.writeTextFile(dest, text);
+  }
+}
+
+function transform(source){
   var typed = {task: "task", note: "tiddler"};
   function drill(node, parent){
     function convert(child){
-      var id = seed++;
-      var item = {
+      const id = next();
+      const item = {
         id: id,
-        $type: typed[child.type] || "task",
-        title: child.text,
+        $type: typed[child["@type"]] || "task",
+        title: child["@text"],
         child: []
       };
-      if (child.tags){
-        item.tag = child.tags.split(",");
+      if (child["@tags"]){
+        item.tag = child["@tags"].split(",");
       }
-      if (child.priority){
-        item.priority = parseInt(child.priority);
+      if (child["@priority"]){
+        item.priority = parseInt(child["@priority"]);
       }
-      if (child.due){
-        item.due = child.due;
+      if (child["@due"]){
+        item.due = child["@due"];
       }
-      if (child.dateModified) {
-        item.modified = child.dateModified;
+      if (child["@dateModified"]) {
+        item.modified = child["@dateModified"];
       }
       parent.child.push(id);
-      var items = drill(child, item);
+      const items = drill(child, item);
       items.unshift(item);
       if (!item.child.length) {
         delete item.child;
       }
       return items;
     }
-    return node.outline ? (node.outline.length ? node.outline.map(convert) : [convert(node.outline)]) : [];
+    return flatten(node.outline ? (node.outline.length ? node.outline.map(convert) : [convert(node.outline)]) : []);
   }
-  var root = {
-    id: seed++,
+  const root = {
+    id: next(),
     $type: "task",
     title: source.opml.head.title,
     child: [],
@@ -48,16 +80,3 @@ function read(source){
   };
   return flatten([root, drill(source.opml.body, root)]);
 }
-
-fs.readFile(filename, {encoding: 'utf-8'}, function(err, opml){
-  if (!err) {
-    var json = JSON.stringify(read(JSON.parse(parser.toJson(opml))), null, "\t");
-    fs.writeFile(destname, json, function(err) {
-      if (err) {
-        console.log("Write failed!", err);
-      }
-    });
-  } else {
-    console.log("Read failed!", err);
-  }
-});
