fce59c71af9e26681e0f6e8a69a8ed2ca0cddfc9
Mario T. Lanza <mlanza@doesideas.com>
2021-07-24 00:25:06 -0400

    revamp find

--- src/work/types/buffer/construct.js
+++ src/work/types/buffer/construct.js
@@ -1,8 +1,10 @@
 import * as _ from "atomic/core";
-
+import {entityWorkspace} from "../entity-workspace/construct.js";
 export function Buffer(repo, workspace){
   this.repo = repo;
   this.workspace = workspace;
 }
 
-export const buffer = _.constructs(Buffer);
+export function buffer(repo, workspace){
+  return new Buffer(repo, workspace || entityWorkspace());
+}
