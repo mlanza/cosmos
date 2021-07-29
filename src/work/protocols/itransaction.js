94583f8c930a265fdcffab379750b6b920b439af
Mario T. Lanza <me@mlanza.com>
2021-07-28 21:58:34 -0400

    drop transaction

--- src/work/protocols/itransaction.js
+++
@@ -1,2 +0,0 @@
-export * from "./itransaction/instance.js";
-export * from "./itransaction/concrete.js";
--- src/work/types/transaction/construct.js
+++
@@ -1,8 +0,0 @@
-import * as _ from "atomic/core";
-
-export function Transaction(buffer, user){
-  this.buffer = buffer;
-  this.user = user;
-}
-
-export const transaction = _.constructs(Transaction);
