750e8423b3adfb8b7244d15f9972d41310c72b44
Mario T. Lanza <me@mlanza.com>
2021-08-07 12:27:52 -0400

    start indexing

--- src/work/types/indexed-entity-workspace/construct.js
+++ src/work/types/indexed-entity-workspace/construct.js
@@ -1,10 +1,11 @@
 import * as _ from "atomic/core";
-import {edgeStore} from "../edge-store/construct.js";
+import * as imm from "atomic/immutables";
 import {entityWorkspace} from "../entity-workspace/construct.js";
 
-export function IndexedEntityWorkspace(indexes, workspace){
+export function IndexedEntityWorkspace(librarian, indexes, workspace){
+  this.librarian = librarian;
   this.indexes = indexes;
   this.workspace = workspace;
 }
 
-export const indexedEntityWorkspace = _.fnil(_.constructs(IndexedEntityWorkspace), edgeStore(), entityWorkspace());
+export const indexedEntityWorkspace = _.fnil(_.constructs(IndexedEntityWorkspace), null, imm.map(), entityWorkspace());
